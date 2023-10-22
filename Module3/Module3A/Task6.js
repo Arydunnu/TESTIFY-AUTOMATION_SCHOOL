f0251252784c9e1eeaf32f9e121416ebfe932dfa
Aridunnu <aridunnuade@gmail.com>
2023-10-22 20:38:28 +0100

    update

--- Module3/Module3A/Task6.js
+++ Module3/Module3A/Task6.js
@@ -1,7 +1,7 @@
-const number = 19
+const number = 14
 if(number % 2) {
-    console.log("the string number is even")
+    console.log("The string number is even")
 }
 else {
-    console.log("number is even")
+    console.log("Number is even")
 }
