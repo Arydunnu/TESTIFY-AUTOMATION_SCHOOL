43334e4ae1d4a2340ea253986f092263ecbbe4da
Aridunnu <aridunnuade@gmail.com>
2023-10-30 17:26:16 +0100

    update task14

---
+++ Module3/Module3A/Task14.js
@@ -0,0 +1,25 @@
+const bookss={
+    
+    title: "How to create value" ,
+
+    description: "This book is all about investing in yourself " ,
+
+    numberOfPages: 20 ,
+
+    author: "Aridunnu" , 
+
+    reading : true ,
+
+    toggleReadingStatus : function(){
+        if(bookss.reading===false){
+            bookss.reading= true
+
+        } else{
+            bookss.reading=false
+        }
+
+}
+
+}
+bookss.toggleReadingStatus()
+console.log (bookss.reading)
