95ecabd4e56c987ed72f37a38fcc3e767952bec6
Aridunnu <aridunnuade@gmail.com>
2023-10-30 05:55:14 +0100

    personal library

---
+++ Module3/Module3A/Task13.js
@@ -0,0 +1,13 @@
+const books={
+    
+    title: "How to create value" ,
+
+    description: "This book is all about investing in yourself " ,
+
+    numberOfPages: 20 ,
+
+    author: "Aridunnu" , 
+
+    reading : true
+
+}
