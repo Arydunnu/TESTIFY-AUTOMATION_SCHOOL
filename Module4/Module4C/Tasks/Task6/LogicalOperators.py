0020f5f2fa9cf2a41887c2d7444a6f68888c3d8c
Aridunnu <aridunnuade@gmail.com>
2024-02-02 20:31:24 +0100

    update tasks

---
+++ Module4/Module4C/Tasks/Task6/LogicalOperators.py
@@ -0,0 +1,10 @@
+Number1 = 70
+Number = 99
+if Number1 > 50 and Number < 100:
+    print("Evaluation is True")
+else:
+    print("Evaluation is False")
+
+Course = "Python"
+if Course == "testify" or "python":
+    print("Yay!! The string is a Testify Python")
---
+++ Module4/Module4C/Tasks/Task7/ForLoop.py
@@ -0,0 +1,3 @@
+number = 10
+for number in range(number):
+    print("Hello World ", number)
